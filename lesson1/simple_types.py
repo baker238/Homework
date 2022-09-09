f6c1209fc61b898e65dca3283633073da8afe6bc
baker238 <112642745+baker238@users.noreply.github.com>
2022-09-09 13:05:43 +0300

    Delete lesson1 directory

--- lesson1/simple_types.py
+++
@@ -1,9 +0,0 @@
-a ='  hi'
-b = 'misha'
-d = 2
-c = f'{a} {b}{d}!    '
-#print(len(c),c.upper(),c.lower(),c.capitalize(),c.strip())
-l ='He33o De3a'.replace('3','l')
-print(l)
-x = None
-print(x is None)
